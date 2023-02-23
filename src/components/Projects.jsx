4ac70ca825affb300a76bf77c2711e577b91ed8a
Chance Ludwick <chanceludwick@gmail.com>
2023-02-22 19:25:18 -0600

    Add hover effect to project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -78,24 +78,22 @@ export default function Projects() {
   return (
     <section id='projects' className='flex justify-center p-8 gap-6'>
       {cardData.map((card, index) => (
-        <div key={index}>
-          <div className='max-w-sm'>
-            <Card imgSrc={card.imgSrc}>
-              <h5 className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-                {card.title}
-              </h5>
-              <p className='font-normal text-gray-700 dark:text-gray-400'></p>
-              <div className='flex flex-wrap gap-2'>
-                {card.badges.map((badge, index) => (
-                  <Badge
-                    key={index}
-                    className={`px-3 ${badge.bg} ${badge.text}`}>
-                    {badge.icon}
-                  </Badge>
-                ))}
-              </div>
-            </Card>
-          </div>
+        <div
+          key={index}
+          className='transition-all max-w-sm hover:scale-110 hover:opacity-20'>
+          <Card imgSrc={card.imgSrc}>
+            <h5 className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+              {card.title}
+            </h5>
+            <p className='font-normal text-gray-700 dark:text-gray-400'></p>
+            <div className='flex flex-wrap gap-2'>
+              {card.badges.map((badge, index) => (
+                <Badge key={index} className={`px-3 ${badge.bg} ${badge.text}`}>
+                  {badge.icon}
+                </Badge>
+              ))}
+            </div>
+          </Card>
         </div>
       ))}
     </section>
