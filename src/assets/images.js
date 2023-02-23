c0b3ba4a0ffe97350b8fb30fb6a10d6555848637
Chance Ludwick <chanceludwick@gmail.com>
2023-02-22 19:18:11 -0600

    Create project cards and badges

--- src/assets/images.js
+++ src/assets/images.js
@@ -7,10 +7,18 @@ import JavaScript from './javascript.svg';
 import React from './react.svg';
 import Tailwind from './tailwind.svg';
 import Node from './node.svg';
+import DebtCalc from './debt-calculator-edited.png';
+import Folio from './folio-edited.png';
+import Battleship from './node-battleship-edited.png';
+import SaaS from './saas-website-edited.png';
 
 export const Images = {
   LudwickLogo,
   HeroImage,
+  DebtCalc,
+  Folio,
+  Battleship,
+  SaaS,
 };
 
 export const Icons = {
@@ -21,4 +29,42 @@ export const Icons = {
   Node,
   Tailwind,
   Bootstrap,
+};
+
+export const Badges = {
+  html: {
+    icon: 'html',
+    bg: 'bg-[#f17844]',
+    text: 'text-[#ffffff]',
+  },
+  css: {
+    icon: 'css',
+    bg: 'bg-[#2965f1]',
+    text: 'text-[#ffffff]',
+  },
+  javascript: {
+    icon: 'javascript',
+    bg: 'bg-[#f0db4f]',
+    text: 'text-[#000000]',
+  },
+  react: {
+    icon: 'react',
+    bg: 'bg-[#61dafb]',
+    text: 'text-[#000000]',
+  },
+  node: {
+    icon: 'node',
+    bg: 'bg-[#339933]',
+    text: 'text-[#ffffff]',
+  },
+  tailwind: {
+    icon: 'tailwind',
+    bg: 'bg-[#38b2ac]',
+    text: 'text-[#ffffff]',
+  },
+  bootstrap: {
+    icon: 'bootstrap',
+    bg: 'bg-[#7952b3]',
+    text: 'text-[#ffffff]',
+  },
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,27 +1,99 @@
-import { Card } from 'flowbite-react';
+import { Badge, Card } from 'flowbite-react';
+import { Images } from '../assets/images';
+import { Badges } from '../assets/images';
 
 const cardData = [
   {
-    imgSrc: 'https://flowbite.com/docs/images/blog/image-1.jpg',
-    title: 'Noteworthy technology acquisitions 2021',
-    description:
-      'Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.',
+    imgSrc: `${Images.Folio}`,
+    title: 'folio',
+    badges: [
+      {
+        icon: 'html',
+        bg: 'bg-[#f17844]',
+        text: 'text-[#ffffff]',
+      },
+      {
+        icon: 'css',
+        bg: 'bg-[#1b73ba]',
+        text: 'text-[#ffffff]',
+      },
+      {
+        icon: 'javascript',
+        bg: 'bg-[#f0db4f]',
+        text: 'text-[#000000]',
+      },
+    ],
+  },
+  {
+    imgSrc: `${Images.DebtCalc}`,
+    title: 'Debt Calculator',
+    badges: [
+      {
+        icon: 'react',
+        bg: 'bg-[#61dafb]',
+        text: 'text-[#ffffff]',
+      },
+      {
+        icon: 'css',
+        bg: 'bg-[#1b73ba]',
+        text: 'text-[#ffffff]',
+      },
+    ],
+  },
+  {
+    imgSrc: `${Images.Battleship}`,
+    title: 'Battleship',
+    badges: [
+      {
+        icon: 'javascript',
+        bg: 'bg-[#f0db4f]',
+        text: 'text-[#000000]',
+      },
+      {
+        icon: 'node',
+        bg: 'bg-[#8cc84b]',
+        text: 'text-[#000000]',
+      },
+    ],
+  },
+  {
+    imgSrc: `${Images.SaaS}`,
+    title: 'SaaS Starter',
+    badges: [
+      {
+        icon: 'html',
+        bg: 'bg-[#f17844]',
+        text: 'text-[#ffffff]',
+      },
+      {
+        icon: 'css',
+        bg: 'bg-[#1b73ba]',
+        text: 'text-[#ffffff]',
+      },
+    ],
   },
 ];
 
 export default function Projects() {
   return (
-    <section id='projects'>
+    <section id='projects' className='flex justify-center p-8 gap-6'>
       {cardData.map((card, index) => (
-        <div key={index} className='p-12 flex justify-center'>
+        <div key={index}>
           <div className='max-w-sm'>
             <Card imgSrc={card.imgSrc}>
               <h5 className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
                 {card.title}
               </h5>
-              <p className='font-normal text-gray-700 dark:text-gray-400'>
-                {card.description}
-              </p>
+              <p className='font-normal text-gray-700 dark:text-gray-400'></p>
+              <div className='flex flex-wrap gap-2'>
+                {card.badges.map((badge, index) => (
+                  <Badge
+                    key={index}
+                    className={`px-3 ${badge.bg} ${badge.text}`}>
+                    {badge.icon}
+                  </Badge>
+                ))}
+              </div>
             </Card>
           </div>
         </div>
